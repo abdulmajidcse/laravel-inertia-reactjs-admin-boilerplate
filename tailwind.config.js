fc2f8d23a74f6133e8c995697a03a08d1595b979
Abdul Majid <abdulmajidcse@gmail.com>
2023-02-15 12:38:48 +0600

    flowbite and flowbite-react install

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "class",
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
         "./resources/js/**/*.jsx",
+        "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
     ],
 
     theme: {
@@ -18,7 +20,7 @@ module.exports = {
     },
 
     plugins: [
-        require("@tailwindcss/forms"),
         require("tailwind-scrollbar")({ nocompatible: true }),
+        require("flowbite/plugin"),
     ],
 };
